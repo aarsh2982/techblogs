d97eb405bd9ed13269e03d502ecd2c01a4ab8832
aarsh29 <invinciblearjuna1@gmail.com>
2024-07-12 15:26:01 +0530

    feat: login signup ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <main>
-      <div>
-        <div className="text-center font-semibold font-mono mt-2 text-xl backdrop">
-          Tech<span className="text-[#3078ff]">Blogs</span>
-        </div>
-      </div>
-    </main>
-  );
+  return <main>Hello This is Tech-Blogs</main>;
 }
